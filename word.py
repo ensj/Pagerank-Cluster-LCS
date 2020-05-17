cc2010a74953e9365015a054e34483ea211d799b
ensj <ensjtrans@gmail.com>
2020-05-18 07:22:45 +0900

    Percent weight + Doubled edges

--- word.py
+++ word.py
@@ -1,11 +1,34 @@
 import os
 import array 
 import re
+import math
 
 from suffix_tree import Tree
 import networkx as nx
 import matplotlib.pyplot as plt
 
+# Proposed by Phil
+def percentweight(f1, f2):
+    #finds %of f1 that is contained in f2
+    #so directed edge in one direction f1->f2
+    #returns (|words contained in f1&f2|/length of f1)
+    #try running both commented and uncommetned on small files
+    n = len(f1)
+    score = 0
+    
+    l1 = [f1[x : x + 1] for x in range(0, len(f1))]
+    tree = Tree({'A': f2})
+    for el in l1:
+        print("l1",l1)
+        print("EL", el)
+            
+            
+        if tree.find(el):
+            print("FOUND")
+            score+=1
+    return score/n
+
+# Proposed by Tran
 def lcsweight(f1, f2):
     k = 1
     w = 0
@@ -19,13 +42,16 @@ def lcsweight(f1, f2):
                 break # if a sequence of k words exists between both files, break the loop and update k
         k *= 2
 
-    return w/log2(n)
+    return w#/math.log(n, 2)
 
 def createNetwork(l):
     G = nx.complete_graph(len(l))
+    G = G.to_directed()
 
     for e in G.edges:
         G[e[0]][e[1]]['weight'] = lcsweight(l[e[0]], l[e[1]])
+        #G[e[1]][e[0]]['weight'] = lcsweight(l[e[1]], l[e[0]]) #meaningless with log alg
+        #print("1: ", G[e[0]][e[1]]['weight'], " 2: ", G[e[1]][e[0]]['weight'])
 
     return G
 
@@ -53,5 +79,5 @@ for filename in os.listdir('input'):
 
 G = createNetwork(words)
 print(names)
-print(nx.pagerank(G))
+#print(nx.pagerank(G))
 drawNetwork(names, G)
